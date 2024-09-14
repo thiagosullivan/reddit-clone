c4e95d2d975e9561675289e9b898a064f32e7862
Thiago Sullivan <thiagosullivan12@gmail.com>
2024-09-14 02:37:21 -0300

    Creating Vote Up and Vote Down function

--- app/actions.ts
+++ app/actions.ts
@@ -5,8 +5,9 @@
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { redirect } from "next/navigation";
 import prisma from "./lib/db";
-import { Prisma } from "@prisma/client";
+import { Prisma, TypeOfVote } from "@prisma/client";
 import { JSONContent} from "@tiptap/react";
+import { revalidatePath } from "next/cache";
 
 export async function updateUsername(prevState: any, formData: FormData){
     const {getUser} = getKindeServerSession();
@@ -143,4 +144,55 @@ export async function createPost({jsonContent}: {jsonContent: JSONContent | null
     });
 
     return redirect("/")
+}
+
+export async function handleVote(formData: FormData) {
+    const { getUser } = getKindeServerSession();
+    const user = await getUser();
+  
+    if (!user) {
+      return redirect("/api/auth/login");
+    }
+  
+    const postId = formData.get("postId") as string;
+    const voteDirection = formData.get("voteDirection") as TypeOfVote;
+  
+    const vote = await prisma.vote.findFirst({
+      where: {
+        postId: postId,
+        userId: user.id,
+      },
+    });
+  
+    if (vote) {
+      if (vote.voteType === voteDirection) {
+        await prisma.vote.delete({
+          where: {
+            id: vote.id,
+          },
+        });
+  
+        return revalidatePath("/", "page");
+      } else {
+        await prisma.vote.update({
+          where: {
+            id: vote.id,
+          },
+          data: {
+            voteType: voteDirection,
+          },
+        });
+        return revalidatePath("/", "page");
+      }
+    }
+  
+    await prisma.vote.create({
+      data: {
+        voteType: voteDirection,
+        userId: user.id,
+        postId: postId,
+      },
+    });
+  
+    return revalidatePath("/", "page");
 }
